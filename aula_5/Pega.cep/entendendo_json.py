faf3e5da717cc873ad52a3d847fcf4ff24787c16
RJSMARTINS <90334848+RJSMARTINS@users.noreply.github.com>
2022-04-30 16:12:26 -0300

    Aula_5

---
+++ aula_5/Pega.cep/entendendo_json.py
@@ -0,0 +1,26 @@
+#Oque é um dicionario
+
+mario = {
+
+"altura": 0,
+"nome": "mario",
+"pode": ["fogo", "capa"],
+"posicao": ["abaixado", "em pé", "montado"],
+
+}
+
+#oque é json
+
+endereco = {
+
+    "cep": "07793-000",
+    "logradouro": "Rua Bebedouro",
+    "complemento": "(Prq Jaraguá)",
+    "bairro": "Panorama (Polvilho)",
+    "localidade": "Cajamar",
+    "uf": "SP",
+    "ibge": "3509205",
+    "gia": "2410",
+    "ddd": "11",
+    "siafi": "6285"
+}
